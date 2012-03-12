8ac6ceb05ebdd3a36d10e8a124c694f38fb83a7c
CRoesinger <c_roesinger@gmx.de>
2012-03-12 15:51:33 -0700

    Update app/models/movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,25 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings
-    ['G', 'PG', 'PG-13', 'R']
+  
+  VALID_SORTS = ["title", "release_date"]
+  
+  scope :order_by, lambda { |sort_column| order("#{sort_column} ASC") }
+  
+  def self.ratings
+    Movie.select('DISTINCT rating').order('rating ASC').collect {|result| result.rating}
+  end
+  
+  def self.filtered(params)
+    valid_ratings = Movie.ratings
+    
+    movies = Movie
+    movies = movies.order_by(params[:sort]) unless params[:sort].blank? or !VALID_SORTS.include?(params[:sort])
+    unless params[:ratings].blank?
+      clauses = []
+      params[:ratings].keys.each do |rating|
+        clauses << "rating = '#{rating}'" if valid_ratings.include?(rating)
+      end
+      movies = movies.where(clauses.join(" OR "))
+    end
+    movies.all
   end
 end
